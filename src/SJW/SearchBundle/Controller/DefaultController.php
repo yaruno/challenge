ed89beacbed9135b63396d5b54ae80dde2e22c49
Jarno Marttila <jarno.marttila@tut.fi>
2013-12-09 15:21:20 +0200

    added map and fixed query

--- src/SJW/SearchBundle/Controller/DefaultController.php
+++ src/SJW/SearchBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace SJW\SearchBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,17 +14,21 @@ class DefaultController extends Controller
     /**
      * @Route("/")
      *
-     * @Template()
+     * @Template("SJWSearchBundle:Default:index.html.twig")
      */
+
     public function indexAction()
     {
-        return array();
+
+
+
+        return array('resp'=>"");
     }
 
     /**
-     * @Route("/api/search")
+     * @Route("/api/search", name="search")
      *
-     * @Template()
+     * @Template("SJWSearchBundle:Default:index.html.twig")
      */
     public function searchAction(Request $request) {
         // Get the search string from the UI.
@@ -32,20 +37,109 @@ class DefaultController extends Controller
         // Read resource file.
         $kernel = $this->get('kernel');
 
-        $filePath = $kernel->locateResource('@SJWSearchBundle/Resources/data/numbers.txt');
+        $filePath = $kernel->locateResource('@SJWSearchBundle/Resources/data/numbers_lat_lon.txt');
 
         // Split lines and comma delimited values.
         $lines = explode("\n", file_get_contents($filePath, true));
 
         $numbers = array();
-
-        foreach($lines as $line) {
-            $numbers[] = explode(';', $line);
-        }
-
+        
         // TODO: Implement search based on query string.
 
+        $results =[];
+
+        $nearest_N = 10;
+
+        foreach($lines as $line) {
+                $temp = explode(';', $line);
+                if(count($temp) == 5){
+                    $unit = array("postcode"=>$temp[0], "city"=>$temp[1], "pop"=>$temp[2],"lat"=>$temp[3],"lon"=>$temp[4]);
+                }
+                $numbers[] = $unit;
+        }
+
+
+        if (is_numeric($searchString)){
+                foreach($numbers as $number){
+                    if ($number['postcode'] == $searchString){
+                        $results = $number;
+                    }
+                }
+            }
+
+            if (is_string($searchString)){
+                foreach($numbers as $number){
+                    if ($number['city'] == $searchString){
+                        $results = $number;
+                    }
+                }
+            }
+           
+            $wanted_results = [];
+
+
+            if (!empty($results)){
+
+
+                foreach ($numbers as $key => $row) {
+                    $postcode[$key]  = $row['postcode'];
+                    $city[$key] = $row['city'];
+                    $pop[$key] = $row['pop'];
+                }
+                // Sort the data with population ascending
+                array_multisort($pop, SORT_ASC, $numbers);
+               
+                $key = array_search($results,$numbers);
+               
+                //check if key + N numbers is beyond array range, adjust accordignly
+
+                if ($key-5 <0 ) {
+
+
+                    for ($i=$key; $i<=$key+10; $i++){
+                        //var_dump($numbers[$i]);
+                        $wanted_results[] = $numbers[$i];
+                        //$var_dump = $wanted_results;
+
+                    }
+
+
+                }
+
+                elseif ($key+5 > count($numbers)) {
+
+                    for ($i=$key-10; $i<=$key; $i++){
+                        //var_dump($numbers[$i]);
+                        $wanted_results[] = $numbers[$i];
+                        //$var_dump = $wanted_results;
+
+                    }
+
+
+
+                }
+
+                else{
+
+                    for ($i=$key-5; $i<=$key+5; $i++){
+                       
+                        $wanted_results[] = $numbers[$i];
+                    
+
+                    }
+
+                }
+               
+
+            }   
+
+
+
+
+        
+       
         // Output content.
-        return new JsonResponse($numbers);
+        return new JsonResponse($wanted_results);
+     
     }
 }
--- src/SJW/SearchBundle/Resources/public/js/search.js
+++ src/SJW/SearchBundle/Resources/public/js/search.js
@@ -3,13 +3,54 @@
         $('.js-do-search').click(function(event) {
             event.preventDefault();
 
+
+
             var search = $('#search-input').val();
+            
 
             $.ajax({
                 url: 'api/search?q=' + search,
                 type: 'GET',
                 success: function(data) {
                     // TODO: implement showing of data.
+
+                    var content = '';
+
+
+                    //If map div exists, remove it and create a new one, set height and other leaflet configs
+                    //add markers from data to map
+                    if ($("#map")){
+                        
+                        $("#map").remove();
+                        $(".results").append("<div id='map'></div>");
+                        $('#map').css('height','300px');
+                    }
+                    var map = L.map('map').setView([61.505, 25], 4);
+
+                    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+                        maxZoom: 18
+                    }).addTo(map);
+
+
+                    //content += '<tbody>'; -- **superfluous**
+                    for (var i = 0; i < data.length; i++) {
+                        content += '<tr>';
+                        content += '<td>' + data[i].postcode + '</td>';
+                        content += '<td>' + data[i].city + '</td>';
+                        content += '<td>' + data[i].pop + '</td>';
+                        content += '</tr>';
+
+
+
+                        var marker_content = "<b>Pop: "+data[i].pop +"</b> <br> Postcode: " + data[i].postcode +"<br> City: "+ data[i].city;
+
+                        var marker = L.marker([data[i].lat, data[i].lon]).bindPopup(marker_content).addTo(map);
+
+
+                    }
+                  
+                     $('#results-table').html(content);
                     console.log(data);
                 }
             });
